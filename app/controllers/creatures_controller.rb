79f4e2aed695729ed11f40715cfc35ef2e13602d
Jess Gardner <inanna1130@gmail.com>
2017-09-06 16:10:58 -0400

    added new, create, edit, update, destroy routes, styling

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -8,6 +8,38 @@ class CreaturesController < ApplicationController
         @creature = Creature.find(params[:id])
     end
 
+    def new
+        @creature = Creature.new
+    end
+
+    def create
+        @creature = Creature.create(creature_params)    
+        
+        if @creature.save
+            redirect_to creature_path(@creature.id)
+        end
+    end
+
+    def edit
+        @creature = Creature.find(params[:id])
+    end
+
+    def update
+        @creature = Creature.find(params[:id])
+        @creature.update(creature_params)
+
+        if @creature.save
+            redirect_to creature_path(@creature.id)
+        end
+    end
+
+    def destroy
+        @creature = Creature.find(params[:id])
+        @creature.destroy
+
+        redirect_to creatures_path
+    end
+
     private
     def creature_params
         params.require(:creature).permit(:name, :description)
